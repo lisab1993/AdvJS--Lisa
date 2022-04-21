4885d1dc6a1119367da6a634569f6617b39801fb
lisab1993 <brown.lisa999@gmail.com>
2022-04-21 13:57:09 -0700

    lab 4 done

---
+++ express/lab5-forum_testing/routes/boardRoutes.js
@@ -0,0 +1,57 @@
+const { Router } = require("express");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
+const Board = require("../models/Board");
+// const Post = require("../models/Post");
+
+// const jwtMiddleware = require("../middleware/jwtMiddleware");
+
+//add jwt back
+const router = Router();
+
+//List all boards
+router.get("/", async (req, res) => {
+  const boards = await Board.find().populate({path: "posts"});
+  res.send(boards);
+});
+
+//Create a board
+router.post("/", jwtMiddleware, async (req, res) => {
+  console.log(req.body, "req.body")
+  const board = new Board(req.body);
+  await board.save();
+  res.send(board);
+});
+
+//Retrieve a specific board
+router.get("/:id", async (req, res) => {
+  const board = await Board.findOne({ _id: req.params.id });
+  res.send(board);
+});
+
+//Update a board
+router.patch("/:id", jwtMiddleware, async (req, res) => {
+  const board = await Board.findOne({ _id: req.params.id }).populate('posts');
+  if (!board) {
+    res.status(404).send("unable to find and update this board!");
+  } else {
+    const boardData = req.body;
+    board.set(boardData);
+    await board.save();
+    res.send(board);
+  }
+});
+
+//Delete a board
+router.delete("/:id", jwtMiddleware, async (req, res) => {
+  const board = await Board.findOne({ _id: req.params.id });
+  if (!board) {
+    res.status(404).send("oops, this board can't be found!");
+  } else if (board.author.toString() !== req.user.id) {
+    res.status(403).send("you can't delete boards you didn't create!");
+  } else {
+    await board.remove();
+    res.send(board);
+  }
+});
+
+module.exports = router;
---
+++ express/lab5-forum_testing/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const AuthRoutes = require("./routes/authRoutes");
+const BoardRoutes = require("./routes/boardRoutes");
+const PostRoutes = require("./routes/postRoutes");
+
+const app = express();
+
+//middleware
+//don't log everything on tests; keeps everything clean
+//another way to write if statement
+if (process.env.ENV !== "test") app.use(morgan("tiny"));
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.use("/auth", AuthRoutes);
+app.use("/board", BoardRoutes);
+app.use("/post", PostRoutes);
+
+const connectDB = async (dbName) => {
+  try {
+    const dbConnection = await mongoose.connect(
+      `mongodb://localhost/${dbName}`
+    );
+    if (process.env.ENV !== "test") {
+      console.log(`connected to ${dbName}`);
+    return dbConnection
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const startServer = async(port = 5050, hostname = 'localhost') => {
+  await connectDB('lisaForumTest5')
+  app.listen(port, hostname, () => {
+    console.log(`Listening at ${hostname}:${port}!!!`);
+  });
+}
+
+
+
+
+
+//for tests
+module.exports = {
+  app,
+  connectDB,
+};
